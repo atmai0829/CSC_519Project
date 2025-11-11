80f66b5c0d91be5cd4fab8824ab4fee81953a5cb
An <atmai@ncsu.edu>
2025-11-10 19:51:59 -0500

    Fixed Linting errors and add a build step to the workflow to build a docker image

--- coffee-project/eslint.config.js
+++ coffee-project/eslint.config.js
@@ -1,35 +1,41 @@
-const js = require("@eslint/js");
+const js = require('@eslint/js');
 
 module.exports = [
   js.configs.recommended,
   {
-    files: ["**/*.js"],
+    files: ['**/*.js'],
     languageOptions: {
       ecmaVersion: 2021,
-      sourceType: "commonjs",
+      sourceType: 'commonjs',
       globals: {
         // Node.js globals
-        process: "readonly",
-        __dirname: "readonly",
-        __filename: "readonly",
-        module: "readonly",
-        require: "readonly",
-        console: "readonly",
+        process: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        console: 'readonly',
+        
+        window: 'readonly',
+        document: 'readonly',
+        alert: 'readonly',
+        axios: 'readonly',
+
         // Mocha globals for tests
-        describe: "readonly",
-        it: "readonly",
-        before: "readonly",
-        after: "readonly",
-        beforeEach: "readonly",
-        afterEach: "readonly",
+        describe: 'readonly',
+        it: 'readonly',
+        before: 'readonly',
+        after: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
       },
     },
     rules: {
-      "no-unused-vars": "warn",
-      "no-console": "off",
-      indent: ["error", 2],
-      quotes: ["error", "single"],
-      semi: ["error", "always"],
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      indent: ['error', 2],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
     },
   },
 ];
